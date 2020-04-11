d9f55bfed5d08783ba76b3e343d9af1e175b8ca7
Alifian <61834423+Alifian771@users.noreply.github.com>
2020-04-11 09:47:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LatihanInheritanceReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Bambang", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Nyonya Puff", 35, 190302, "Guru Ekonomi");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Patrick", 21, 10506, "patrickstar@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
